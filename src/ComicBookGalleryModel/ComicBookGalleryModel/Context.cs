cc425ea2c70284e6ad03b49d4433fff4d2a8ee45
EGrayTech <EGrayTech@users.noreply.github.com>
2018-03-15 01:44:02 -0400

    Seeding database with test data.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using ComicBookGalleryModel.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ComicBookGalleryModel
 {
@@ -14,9 +15,19 @@ namespace ComicBookGalleryModel
         {
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
     }
 }
